f13ae735e41d9ba5550d8a46d3af88995d09b4fb
Joseph <113585973+phaviaecom@users.noreply.github.com>
2022-10-01 02:16:45 +0700

    Update code for change variant collection and recommendations

--- assets/customize.js
+++ assets/customize.js
@@ -1,28 +1,90 @@
 $(document).ready(() => {
-	if(window.location.href.includes("/collections")){
 
+    let setVariants = () => {
 		if( $('.list-variants .swatch-item').length > 0 ){
-
 			$('.list-variants .swatch-item').each( (idx, el) => {
 				$(el).find('a').on('click', (e) => {
-					console.log(e.currentTarget);
 					let productId  = e.currentTarget.dataset.id;
 					let featureImg = e.currentTarget.dataset.featured;
 					let secondImg  = e.currentTarget.dataset.second;
 					let handleProduct = e.currentTarget.dataset.handle;
+					let parentNode = $(e.currentTarget).parent().parent().parent().parent();
 
-					$(`#list_variants-${productId } .swatch-item`).removeClass('is-selected');
+					$(parentNode).find('.list-variants .swatch-item').removeClass('is-selected');
 					$(e.currentTarget.parentNode).addClass('is-selected');
 
-                	// Change the attributes of the image
-					$(`#feature-img-${productId}`).attr('srcset', `${featureImg}`);
-					$(`#feature-img-${productId}`).attr('data-srcset', `${featureImg}`);
-					$(`#second-img-${productId}`).attr('srcset', `${secondImg}`);
-					$(`#second-img-${productId}`).attr('data-srcset', `${secondImg}`);
-					$(`.product-link-${productId}`).attr('href', `/products/${handleProduct}`);
+					// Change the attributes of the image
+					$(parentNode).find('.featured-image').attr('srcset', `${featureImg}`);
+					$(parentNode).find('.featured-image').attr('data-srcset', `${featureImg}`);
+
+					$(parentNode).find('.secondary-image').attr('srcset', `${secondImg}`);
+					$(parentNode).find('.secondary-image').attr('data-srcset', `${secondImg}`);
+
+					$(parentNode).find('.grid-product__link').attr('href', `/products/${handleProduct}`);
 				})
 			})
 		}
-
 	}
+
+	if(window.location.href.includes("/products")){
+		let setTime = setInterval(() => {
+			$('.list-variants .swatch-item').each( (idx, el) => {
+					$(el).find('a').on('click', (e) => {
+						console.log(e.currentTarget);
+						let productId  = e.currentTarget.dataset.id;
+						let featureImg = e.currentTarget.dataset.featured;
+						let secondImg  = e.currentTarget.dataset.second;
+						let handleProduct = e.currentTarget.dataset.handle;
+						let parentNode = $(e.currentTarget).parent().parent().parent().parent();
+	
+						$(parentNode).find('.list-variants .swatch-item').removeClass('is-selected');
+						$(e.currentTarget.parentNode).addClass('is-selected');
+	
+						// Change the attributes of the image
+						$(parentNode).find('.featured-image').attr('srcset', `${featureImg}`);
+						$(parentNode).find('.featured-image').attr('data-srcset', `${featureImg}`);
+	
+						$(parentNode).find('.secondary-image').attr('srcset', `${secondImg}`);
+						$(parentNode).find('.secondary-image').attr('data-srcset', `${secondImg}`);
+	
+						$(parentNode).find('.grid-product__link').attr('href', `/products/${handleProduct}`);
+					})
+				})
+		}, 1000);
+
+		setTimeout(function () {
+			clearInterval(setTime);
+		}, 6000);
+
+	 }else if( window.location.href.includes("/collections")){
+		let setTime = setInterval(() => {
+			$('.list-variants .swatch-item').each( (idx, el) => {
+					$(el).find('a').on('click', (e) => {
+						console.log(e.currentTarget);
+						let productId  = e.currentTarget.dataset.id;
+						let featureImg = e.currentTarget.dataset.featured;
+						let secondImg  = e.currentTarget.dataset.second;
+						let handleProduct = e.currentTarget.dataset.handle;
+						let parentNode = $(e.currentTarget).parent().parent().parent().parent();
+	
+						$(parentNode).find('.list-variants .swatch-item').removeClass('is-selected');
+						$(e.currentTarget.parentNode).addClass('is-selected');
+	
+						// Change the attributes of the image
+						$(parentNode).find('.featured-image').attr('srcset', `${featureImg}`);
+						$(parentNode).find('.featured-image').attr('data-srcset', `${featureImg}`);
+	
+						$(parentNode).find('.secondary-image').attr('srcset', `${secondImg}`);
+						$(parentNode).find('.secondary-image').attr('data-srcset', `${secondImg}`);
+	
+						$(parentNode).find('.grid-product__link').attr('href', `/products/${handleProduct}`);
+					})
+				})
+		}, 1500);
+	 }
+	 else{
+
+		setVariants();
+	 }
+
 })
